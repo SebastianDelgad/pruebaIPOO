27b089be4f3cdfa4438220a7570278a10904b384
luisalvaran17 <luis.albaran@correounivalle.edu.co>
2017-02-18 12:25:11 -0500

    agrego archivo

---
+++ sumasimple.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	int x;
+	int y;
+	
+	cout << "ingrese x" << endl;
+	cin >> x;
+	
+	cout << "ingrese y" << endl;
+	cin >> y;
+	
+	if (x > y){ 
+		cout << (x + y) << endl; 
+			}
+			
+			else {
+				cout << (x-y) << endl;
+				}
+				
+	
+} 
